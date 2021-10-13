f663bab6bb01fcaa726653aa0e35b40038fe44c3
Henrique Serres <henrique@gmail.com>
2021-10-13 19:52:21 -0300

    exercicios 4.2

---
+++ fundamentos/bloco-4-introducao-javascript/dia2-javascript/ex4.js
@@ -0,0 +1,16 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let somaNumbers = 0;
+let mediaNumbers = 0;
+
+ for (let index = 0; index < numbers.length; index += 1) {
+   somaNumbers += numbers[index];
+ }
+ mediaNumbers = somaNumbers / numbers.length;
+
+ if (mediaNumbers > 20) {
+   console.log("Valor maior que 20");
+
+ } else {
+   console.log("Valor menor ou igual a 20")
+ }
+ 
---
+++ fundamentos/bloco-4-introducao-javascript/dia2-javascript/ex6.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impares = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] / 2 !== 0) {
+  impares += 1;
+  }
+}
+if (impares === 0) {
+  console.log('Nenhum valor ímpar')
+} else {
+  console.log(impares);
+}
